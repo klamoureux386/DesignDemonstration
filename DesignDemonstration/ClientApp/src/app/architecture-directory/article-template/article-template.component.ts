aad6166ef43c691e9352feb68e6a603a898826f3
Kyle Lamoureux <klamoureux386@gmail.com>
2023-10-17 01:12:33 -0400

    move to InsertAdjacentHtml over innerHtml

--- DesignDemonstration/ClientApp/src/app/architecture-directory/article-template/article-template.component.ts
+++ DesignDemonstration/ClientApp/src/app/architecture-directory/article-template/article-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ArticleClient, ArticleDTO } from 'src/api.generated.clients';
 
 @Component({
@@ -8,23 +8,30 @@ import { ArticleClient, ArticleDTO } from 'src/api.generated.clients';
   encapsulation: ViewEncapsulation.None,
   providers: [ArticleClient]
 })
-export class ArticleTemplateComponent implements OnInit {
+export class ArticleTemplateComponent implements AfterViewInit {
 
   public article: ArticleDTO = new ArticleDTO();
+  //Swap to viewChild?
+  public templateDiv: HTMLElement | null = null;
 
   constructor(private articleClient: ArticleClient) {
 
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.templateDiv = document.getElementById('templateDiv')
     this.getArticle(1);
   }
 
   getArticle(id: number) {
-    this.articleClient.get(id).subscribe(res => {
+    this.articleClient.get(id).subscribe({
+    next: (res) => {
       this.article = res
       this.insertImgElements();
-    })
+      this.templateDiv!.insertAdjacentHTML('beforeend', this.article.text);
+    },
+    error: (error) => this.templateDiv?.insertAdjacentHTML('beforeend', "Sorry, an error occurred while loading your article!")
+    });
   }
 
   insertImgElements() {
